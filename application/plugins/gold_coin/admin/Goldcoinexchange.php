187efbdd92ebaaab657d6a3b5d453efc85b90444
nixiaozi <776493846@qq.com>
2021-07-25 21:40:08 +0800

    添加后台审核并添加钱包金额

--- application/plugins/gold_coin/admin/Goldcoinexchange.php
+++ application/plugins/gold_coin/admin/Goldcoinexchange.php
@@ -2,7 +2,10 @@
 namespace app\plugins\gold_coin\admin;
 
 use app\plugins\gold_coin\service\GoldCoinBaseService;
+use app\plugins\gold_coin\service\GoldCoinService;
 use app\plugins\gold_coin\service\GoldExchangeService;
+use app\plugins\wallet\service\WalletService;
+use app\service\IntegralService;
 use think\Controller;
 
 class Goldcoinexchange extends Controller
@@ -50,6 +53,50 @@ class Goldcoinexchange extends Controller
         // 参数
         $this->assign('params', $params);
         return $this->fetch('../../../plugins/view/gold_coin/admin/goldcoinexchange/index');
+    }
+
+    public function auditinfo($params = [])
+    {
+        $data = [];
+        if(!empty($params['id']))
+        {
+            $data_params = array(
+                'm'         => 0,
+                'n'         => 1,
+                'where'     => ['id'=>intval($params['id'])],
+            );
+            $ret = GoldCoinBaseService::ExchangeList($data_params);
+            if(!empty($ret['data'][0]))
+            {
+                // 用户金币
+                $user_goldcoin = GoldCoinService::UserGoldCoin($ret['data'][0]['user_id']);
+                // 用户积分
+                $user_integral = IntegralService::UserIntegral($ret['data'][0]['user_id']);
+
+
+                if($user_goldcoin['code'] == 0)
+                {
+                    $data = $ret['data'][0];
+                    $this->assign('user_goldcoin', $user_goldcoin['data']);
+                    $this->assign('user_integral', $user_integral['integral']);
+                }else{
+                    $this->assign('msg', '用户钱包与用户积分系统异常');
+                }
+            }else {
+                $this->assign('msg', '数据不存在或已删除');
+            }
+        }else {
+            $this->assign('msg', '参数id有误');
+        }
+
+        
+        $this->assign('data', $data);
+        return $this->fetch('../../../plugins/view/gold_coin/admin/goldcoinexchange/auditinfo');
+    }
+
+    public function audit($params = [])
+    {
+        return GoldExchangeService::ExchangeAudit($params);
     }
 }
 ?>
--- application/plugins/gold_coin/service/GoldExchangeService.php
+++ application/plugins/gold_coin/service/GoldExchangeService.php
@@ -3,7 +3,12 @@ namespace app\plugins\gold_coin\service;
 
 
 use think\Db;
-use app\plugins\gold_coin\service\GoldCoinBaseService;
+use app\plugins\gold_coin\service\GoldCoinService;
+use app\plugins\wallet\service\WalletService;
+use app\service\IntegralService;
+use app\service\MessageService;
+use base\Uploader;
+use think\db\Where;
 
 class GoldExchangeService
 {
@@ -82,7 +87,177 @@ class GoldExchangeService
 
     }
 
+    public static function ExchangeAudit($params=[])
+    {
+        // 参数验证
+        $p = [
+            [
+                'checked_type'      => 'empty',
+                'key_name'          => 'id',
+                'error_msg'         => '提现id有误',
+            ],
+            [
+                'checked_type'      => 'length',
+                'key_name'          => 'msg',
+                'checked_data'      => '180',
+                'error_msg'         => '备注最多 180 个字符',
+            ],
+            [
+                'checked_type'      => 'in',
+                'key_name'          => 'type',
+                'checked_data'      => ['agree', 'refuse'],
+                'error_msg'         => '操作类型有误，同意或拒绝操作出错',
+            ],
+        ];
+        $ret = ParamsChecked($params, $p);
+        if($ret !== true)
+        {
+            return DataReturn($ret, -1);
+        }
 
+        // 获取要兑现的数据
+        $exchange = Db::name('PluginsGoldCoinMoney')->find(intval($params['id']));
+        if(empty($exchange))
+        {
+            return DataReturn('兑换数据不存在或已删除', -10);
+        }
+
+        // 状态
+        if($exchange['status'] != 0)
+        {
+            return DataReturn('状态不可操作['.self::$Excharge_status_list[$exchange['status']]['name'].']', -11);
+        }
+
+        // 兑换金额处理
+        $exchange_money =PriceNumberFormat($exchange['money_real']); 
+        // 用户金币
+        $user_goldcoin = GoldCoinService::UserGoldCoin($exchange['user_id'])['data']['current_coin'];
+        // 用户积分
+        $user_integral = IntegralService::UserIntegral($exchange['user_id'])['integral'];
+
+        if( $exchange_money <= 0.00 || $exchange_money > $user_goldcoin 
+            || $exchange_money > $user_integral)
+        {
+            return DataReturn('打款金额有误,已经超出了可兑现范围', -12);
+        }
+
+
+        // 获取用户钱包
+        $wallet = WalletService::UserWallet($exchange['user_id']);
+        if(empty($wallet))
+        {
+            return DataReturn('用户钱包不存在或已删除', -20);
+        }
+
+        
+        // 是否发送消息
+        $is_send_message = (isset($params['is_send_message']) && $params['is_send_message'] == 1) ? 1 : 0;
+
+        // 开始处理
+        Db::startTrans();
+
+
+        // 需要修改的提现信息更新
+        $exchange_data = [
+            'status'    => $params['type'] == 'agree'?2:-2,
+            'msg'       => empty($params['msg']) ?
+                ($params['type'] == 'agree'?'恭喜你，你的兑现申请已通过':'对不起，你的兑现申请被拒绝' ): $params['msg'],
+            'upd_time'  => time()
+        ];
+
+        DB::name('PluginsGoldCoinMoney')->where(['id' =>$exchange['id']])->update($exchange_data);
+
+        // 需要修改减少用户积分，添加用户积分日志
+        $user_integral = Db::name('User')->where(['id'=>$exchange['user_id']])->value('integral');
+        if($user_integral<$exchange['integral_out'] || 
+            !Db::name('User')->where(['id'=>$exchange['user_id']])->setDec('integral', $exchange['integral_out']))
+        {
+            Db::rollback();
+            return DataReturn('用户积分抵扣失败,检查积分是否剩余足够', -10);
+        }
+
+        if(!IntegralService::UserIntegralLogAdd($exchange['user_id'],$user_integral,$exchange['integral_out'],'用户兑现抵扣积分'))
+        {
+            // 如果为False 就表示添加不成功
+            Db::rollback();
+            return DataReturn('用户积分抵扣失败,请检查后重试', -10);
+        }
+
+        // 需要减少用户金币， 并添加用户金币日志
+        $goldcoin = GoldCoinService::UserGoldCoin($exchange['user_id']);
+        $coindata = [
+            'current_coin' => $goldcoin['data']['current_coin']- $exchange['gold_out'],
+            'used_coin' => $goldcoin['data']['used_coin']+ $exchange['gold_out'],
+            'upd_time'  => time(),
+        ];
+        if(!Db::name('PluginsGoldCoin') ->
+            where(['id'=>$goldcoin['data']['id']])->update($coindata))
+        {
+            Db::rollback();
+            return DataReturn('金币账户操作失败', -100);
+        }
+
+        $coinlog_data = [
+            'user_id'           => $goldcoin['data']['user_id'],
+            'gold_coin_id'         => $goldcoin['data']['id'],
+            'business_type'     => 1,
+            'operation_type'    => 0,
+            'operation_coin'   => $exchange['gold_out'],
+            'original_coin'    => $goldcoin['data']['current_coin'],
+            'latest_coin'      => $coindata['current_coin'],
+            'msg'  => '金币账户兑现'.' [ 用户金币减少'.$exchange['gold_out'].'个 ]'
+        ];
+        if(!GoldCoinService::GoldCoinLogInsert($coinlog_data))
+        {
+            Db::rollback();
+            return DataReturn('用户金币账户日志添加失败', -101);
+        }
+
+        // 需要增加用户钱包余额，并且添加 用户钱包日志
+        $wallet = Db::name('PluginsWallet')->where(['user_id' =>$exchange['user_id'] ])->find();
+        $userwalletdata = [
+            'normal_money'      => $wallet['normal_money'] + $exchange['money_real'],
+        ];
+
+        if(!Db::name('PluginsWallet')->where(['user_id' =>$exchange['user_id']])->update($userwalletdata))
+        {
+            Db::rollback();
+            return DataReturn('操作失败', -100);
+        }
+
+        // 添加日志
+        $walletlog_data = [
+            'user_id'           => $wallet['user_id'],
+            'wallet_id'         => $wallet['id'],
+            'business_type'     => 0,
+            'operation_type'    => 1,
+            'money_type'        => 0,
+            'operation_money'   => PriceNumberFormat($exchange['money_real']),
+            'original_money'    => $wallet['normal_money'],
+            'latest_money'      => $userwalletdata['normal_money'],
+        ];
+        $walletlog_data['msg'] = '金币兑现钱包余额'.' [ 增加金额'.$walletlog_data['operation_money'].'元 ]';
+        if(!WalletService::WalletLogInsert($walletlog_data))
+        {
+            Db::rollback();
+            return DataReturn('钱包日志添加失败', -101);
+        }
+
+        
+        // 消息通知
+        if($is_send_message == 1)
+        {
+            MessageService::MessageAdd($exchange['user_id'], '金币兑现审核', $exchange_data['msg'], GoldCoinBaseService::$business_type_name, 1);
+        }
+
+
+        // 处理成功
+        Db::commit();
+        return DataReturn('操作成功', 0);
+
+
+
+    }
 
 }
 
